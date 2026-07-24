505b45893ca356c1d7a157c862a286e0876dcd7a
Will-R <williamrashad.ych@gmail.com>
2026-07-24 17:59:30 -0400

    your message here

--- code/run_si.sh
+++ code/run_si.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH --account=ctb-simine
 #SBATCH --time=01:00:00
-#SBATCH --ntasks=1
-#SBATCH --mem=4G
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=4G
+#SBATCH --nodes=1
 
 module load gcc openmpi flexiblas fftw eigen voro++
 export PATH=$HOME/software/lammps-mtp-cpu/bin:$PATH
 
-lmp -in npt_si_300k.in
+cd melt
+srun lmp -in melt.in
