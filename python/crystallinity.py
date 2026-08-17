14df0c22032fe33b3567fb9dc168856465b462e9
Will-R <williamrashad.ych@gmail.com>
2026-08-17 17:16:13 -0400

    your message here

--- python/crystallinity.py
+++ python/crystallinity.py
@@ -6,3 +6,10 @@ print("mean classified:", (216 - other).mean())
 print("mean crystallinity %:", 100 * (216 - other).mean() / 216)
 print("frames with any:", np.mean(other < 216) * 100, "%")
 print("frames with hexagonal:", np.sum(hexd > 0))
+
+import itertools
+on = d[:, 2] < 216
+runs = [(k, len(list(g))) for k, g in itertools.groupby(on)]
+print("longest on:", max(l for k, l in runs if k))
+print("mean on:", np.mean([l for k, l in runs if k]))
+print("number of runs:", len(runs))
